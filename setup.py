53241f670e7eedb0e57084670636e1abaef2d640
MadeYoga <vngla21@gmail.com>
2020-04-09 01:19:33 +0700

    Add setup & requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='ijazahpy',
+      version='0.0.1',
+      description='Segmentasi Ijazah Indonesia',
+      url='https://github.com/madeyoga/ijazahpy',
+      author='Yeogaa',
+      author_email='m26416083@john.petra.ac.id',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
