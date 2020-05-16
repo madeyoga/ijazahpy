33b1909ff9bb30dbec61570db0a7298abe6f1a63
MadeYoga <vngla21@gmail.com>
2020-05-16 13:17:39 +0700

    Update setup ver 0.2.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='ijazahpy',
-      version='0.2.0',
+      version='0.2.2',
       description='Segmentasi Ijazah Indonesia',
       url='https://github.com/madeyoga/ijazahpy',
       author='Yeogaa',
